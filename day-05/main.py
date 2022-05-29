df775051f0547df9c9571f56b3ebb8e8596e657f
EloyChavo <andrealves431@hotmail.com>
2022-05-29 20:46:20 -0300

    Day 05 - Completed

---
+++ day-05/main.py
@@ -0,0 +1,31 @@
+import random
+
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v',
+           'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R',
+           'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+print("Welcome to the PyPassword Generator!")
+nr_letters = int(input("How many letters would you like in your password?\n"))
+nr_symbols = int(input(f"How many symbols would you like?\n"))
+nr_numbers = int(input(f"How many numbers would you like?\n"))
+password_len = nr_letters + nr_symbols + nr_numbers
+
+# Hard Level - Order of characters randomised:
+# e.g. 4 letter, 2 symbol, 2 number = g^2jk8&P
+
+random_password = ''
+count = 0
+for i in range(0, password_len + 1):
+    if i < nr_letters:
+        random_password += random.choice(letters)
+    elif nr_letters < i <= nr_symbols + nr_letters:
+        random_password += random.choice(symbols)
+    elif nr_letters + nr_symbols < i <= nr_numbers + nr_letters + nr_symbols:
+        random_password += random.choice(numbers)
+
+random_password = list(random_password)
+random.shuffle(random_password)
+shuffled_password = ''.join(random_password)
+print(f"\n{shuffled_password}")
